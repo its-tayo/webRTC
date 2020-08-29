ceb88fd3798e00f8d6ae516c76112e1cbbf80f21
its-tayo <akindolietayo@gmail.com>
2020-08-29 18:56:10 +0100

    feat: add constraints

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-// get appropriate platform API
+// get appropriate user agent API
 navigator.getUserMedia =
   navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia;
 
-// set constraints
-const constraints = { audio: false, video: true };
-
 // get video
 const video = document.querySelector("#video");
+
+// get btns
+const vgaButton = document.querySelector("#vga");
+const qvgaButton = document.querySelector("#qvga");
+const hdButton = document.querySelector("#hd");
 
 // getUserMedia success callback
 const successFn = (stream) => {
@@ -21,4 +23,47 @@ const failureFn = (error) => {
   console.log(error);
 };
 
-navigator.getUserMedia(constraints, successFn, failureFn);
+// Constraints object for low resolution video
+const qvgaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 320,
+      maxHeight: 240,
+    },
+  },
+};
+
+// Constraints object for standard resolution video
+const vgaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 640,
+      maxHeight: 480,
+    },
+  },
+};
+
+// Constraints object for high resolution video
+const hdConstraints = {
+  video: {
+    mandatory: {
+      minWidth: 1280,
+      minHeight: 960,
+    },
+  },
+};
+
+// Associate actions with buttons:
+qvgaButton.onclick = () => {
+  getMedia(qvgaConstraints);
+};
+vgaButton.onclick = () => {
+  getMedia(vgaConstraints);
+};
+hdButton.onclick = () => {
+  getMedia(hdConstraints);
+};
+
+const getMedia = (constraints) => {
+  navigator.getUserMedia(constraints, successFn, failureFn);
+};
